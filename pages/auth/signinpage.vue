11b6652968d465aed6a3d8c11b8de78ab7b61039
itsme-CJ <crisjohnsalazar101@gmail.com>
2025-07-07 10:44:31 +0800

    added my project

---
+++ pages/auth/signinpage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="d-flex align-center justify-center" style="height: 100vh;">
+        <v-card width="550" class="pa-8 text-center">
+            <h1 class="ay=">Sign In Page</h1>
+
+            <v-form>
+
+                <v-text-field label="Email" outlined prepend-inner-icon="mdi-account"></v-text-field>
+                <v-text-field label="Password" outlined prepend-inner-icon="mdi-lock" type="password"></v-text-field>
+                <v-btn color="primary" block>Login</v-btn>
+                <p class="my-5">OR</p>
+                <v-btn color="red" block @click="signInWithGoogle()"><v-icon left>mdi-google</v-icon> Sign IN With Google </v-btn>
+                <p class="my-3"></p>
+                <v-btn color="blue" block><v-icon left>mdi-facebook</v-icon> Sign IN With Facebook </v-btn>
+            </v-form>
+            
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: "auth",
+    middleware: 'guest',
+    methods: {
+        signInWithGoogle() {
+            this.$auth.loginWith('google')
+        }
+    }
+
+}
+</script>
