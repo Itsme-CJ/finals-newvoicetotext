4b472e0f8db6ee3e61b78a89cdb2706ca36457c5
itsme-CJ <crisjohnsalazar101@gmail.com>
2025-07-28 08:46:35 +0800

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - BSIT-3A',
-    title: 'QR Code Scanner',
+    title: 'Voice to Text',
     htmlAttrs: {
       lang: 'en'
     },
